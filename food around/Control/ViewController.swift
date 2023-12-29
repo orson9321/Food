02faa2bf3edd8a0238d74526d042838157fcff6d
orson <orson9321@gmail.com>
2023-12-29 15:39:30 -0500

    update zoom in out

--- food around/Control/ViewController.swift
+++ food around/Control/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     // TODO this shouldn't be a constant, should depends on recipe location/distribution
     let distance_span: CLLocationDistance = 5000
-    
-    
+
     func initialize()
     {
         mapView.delegate = self
@@ -26,15 +25,25 @@ class ViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         // assign all delegates
         initialize()
-        // load country pins
+        // load nation pins
         loadNations()
+        
+        
+        //loadRecipeLocs()
     }
     
-    // load all country pins
+    // Load all nation pins
     func loadNations()
     {
         for idx in nation_storage.indices {
             mapView.addAnnotation(nation_storage[idx])
+        }
+    }
+    
+    func loadRecipeLocs()
+    {
+        for recipe_loc in recipe_storage {
+            mapView.addAnnotation(recipe_loc.value)
         }
     }
     
@@ -58,9 +67,9 @@ class ViewController: UIViewController, MKMapViewDelegate {
         return CLLocationCoordinate2D(latitude: latitude_sum / Double(ids.count), longitude: longtitude_sum / Double(ids.count))
     }
     
-    // called when an annotation is selected.
-    // Question: how to differentiate 2 lvls of pins
+    // Called when an annotation is selected.
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("annotation selected")
         if let annotationTitle = view.annotation?.title
         {
             if let nation = view.annotation! as? Nation {
@@ -68,18 +77,57 @@ class ViewController: UIViewController, MKMapViewDelegate {
                 // zoom to selected location
                 let zoomed_region = MKCoordinateRegion(center: calcRecipeLocCenter(ids: recipe_pin_ids), latitudinalMeters: distance_span, longitudinalMeters: distance_span)
                 mapView.setRegion(zoomed_region, animated: true)
-              
-                // hide name pins
-                view.isHidden = true
                 // display recipe pins
+                
+                // make a back button. When clicked, all recipe loc annotations get distroyed
                 loadRecipeLocs(ids: recipe_pin_ids)
             }
             else
             {
                 print("recipe loc pin")
                 // display recipe overlay
+                
+                // add buttom bar
+                
+                // recipe list overlay
             }
         }
+    }
+    
+    // called when visible region changes
+    func mapView(
+        _ mapView: MKMapView,
+        regionDidChangeAnimated animated: Bool
+    )
+    {
+        let annotations = mapView.annotations
+
+            for annotation in annotations
+            {
+                if (mapView.region.span.latitudeDelta > 30)
+                {
+                    mapView.view(for: annotation)?.isHidden = true
+                    
+                    if let nation = annotation as? Nation
+                    {
+                        mapView.view(for: annotation)?.isHidden = false
+                    }
+                    else if let recipe_loc = annotation as? RecipeLoc
+                    {
+                        mapView.view(for: annotation)?.isHidden = true
+                    }
+                }
+                else {
+                    if let nation = annotation as? Nation
+                    {
+                        mapView.view(for: annotation)?.isHidden = true
+                    }
+                    else if let recipe_loc = annotation as? RecipeLoc
+                    {
+                        mapView.view(for: annotation)?.isHidden = false
+                    }
+                }
+            }
     }
 }
 
